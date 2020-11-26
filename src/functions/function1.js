998bc25b8473c137ef951c4bbcda72e59946a2a2
Georgios Pilitsoglou <george.pilitsoglou@gmail.com>
2020-11-26 18:08:06 +0200

    added german sayHi

--- src/functions/function1.js
+++ src/functions/function1.js
@@ -1,19 +1,21 @@
 (function() {
   // JavaScript functions behave like any other data type in the language;
   // we can assign functions to variables, and we can reassign them to new variables.
-
   function sayHi(message) {
-    // console.log("Coming from sayHi? " + message);
+    console.log("Coming from sayHi? " + message);
   }
 
-  sayHi("Hi");
-
   const geiaSou = sayHi;
+  const sageHallo = geiaSou;
 
-  geiaSou("Geia Sou");
+  sageHallo("  Hallo  ");
+  sayHi("Geia Sou");
+  geiaSou(" Hi ");
 
   function multiplyGenerator(multiplier) {
+
     console.log("The multiplier is --> ", multiplier);
+
     function multiplierFunction(numberToMultiply) {
       console.log("The numberToMultiply is --> ", numberToMultiply);
       return multiplier * numberToMultiply;
@@ -25,6 +27,4 @@
   const multiplyWithFive = multiplyGenerator(5);
 
   console.log(multiplyWithFive(8));
-
-
 })();
