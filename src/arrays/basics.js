c3ac4fd5f4eb9b25b45e779c25615da93145f283
Georgios Pilitsoglou <george.pilitsoglou@gmail.com>
2020-11-27 19:51:13 +0200

    added another challenge at arrays

--- src/arrays/basics.js
+++ src/arrays/basics.js
@@ -9,13 +9,15 @@ example1.forEach((element, i) => {
   console.log(element, i);
 });
 
-console.log(example1);
+// Challenge 1
+// Given a list ['A', 'B', 'C']; write commands so the list becomes ['A', 'D', 'C']
 
-// Todo
+// Challenge 2
 // Na ginetai console me Result --> 1os einai Fotis, 2os einai Alexandra kok
 const people = ['Fotis', 'Alexandra', 'Giannis', 'Aggelos', 'Georgios'];
+console.log(people);
 
-// Challenge - Manage Inventory
+// Challenge 3 - Manage Inventory
 
 /*
   1. Your company is launching 3 new products:
