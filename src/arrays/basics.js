c8fce7ccebc22249e57fa7b1553619dca3fc04f1
Georgios Pilitsoglou <george.pilitsoglou@gmail.com>
2020-11-27 15:57:11 +0200

    added example from scrimba fo the arrays basic js

---
+++ src/arrays/basics.js
@@ -0,0 +1,12 @@
+const example1 = [5, 7, 6];
+
+example1.push(8, 9, 10); // adds 8, 9, 10
+example1.pop(); // removes last here is 10
+
+example1[0] = 1; // instead of 5 is 1
+
+example1.forEach((element) => {
+  console.log(element);
+});
+
+console.log(example1);
