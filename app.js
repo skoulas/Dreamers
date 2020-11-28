5fd708ff0116516a7bebac1bd92625a10ea6017d
Georgios Pilitsoglou <george.pilitsoglou@gmail.com>
2020-11-28 11:29:30 +0200

    added testing example file

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import './src/testing/index.js';
 import './src/basics/index.js';
 import './src/arrays/index.js';
 import './src/functions/index.js';
---
+++ src/testing/example.js
@@ -0,0 +1,7 @@
+function sum(a, b) {
+  return a + b + 2;
+}
+
+const addition = sum(1, 2); // 3
+
+console.log(addition);
