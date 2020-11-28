0392981b8c8fd7b3ec0469a27b6d585ccd7e5e8b
Georgios Pilitsoglou <george.pilitsoglou@gmail.com>
2020-11-28 12:37:21 +0200

    updated test

--- src/testing/example.spec.js
+++ src/testing/example.spec.js
@@ -2,12 +2,12 @@ const isPersonAdult = require('./example');
 
 describe('isPersonAdult function', () => {
   test('it should return true if the personAge is adult', () => {
-    expect(isPersonAdult(18)).toBeTruthy();
-    expect(isPersonAdult(25)).toBeTruthy();
+    expect(isPersonAdult(18)).toBe(true);
+    expect(isPersonAdult(25)).toBe(true);
   });
 
   test('it should return false if the personAge is not adult', () => {
-    expect(isPersonAdult(14)).toBeFalsy();
-    expect(isPersonAdult(17)).toBeFalsy();
+    expect(isPersonAdult(14)).toBe(false);
+    expect(isPersonAdult(17)).toBe(false);
   });
 });
