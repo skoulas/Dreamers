cc5bead32fa2b3e01fd3beb7d19930c69e663701
Georgios Pilitsoglou <george.pilitsoglou@gmail.com>
2020-12-31 10:03:12 +0200

    small refactor to fotis test

--- src/exercises/fotis/strings.spec.js
+++ src/exercises/fotis/strings.spec.js
@@ -2,12 +2,18 @@ import { convertStringToArray, convertArrayToString } from './strings';
 
 describe('convertArrayToString function', () => {
   test('it should return a string from an array', () => {
-    expect(convertArrayToString(['i', 'love', 'tacos'])).toBe(['i love tacos']);
+    expect(convertArrayToString(['i', 'love', 'tacos'])).toEqual(
+      'i love tacos'
+    );
   });
 });
 
 describe('convertStringToArray function', () => {
-  test('it should return a string from an array', () => {
-    expect(convertStringToArray('i love tacos')).toBe([['i', 'love', 'tacos']]);
+  test('it should return an array from a string', () => {
+    expect(convertStringToArray('i love tacos')).toEqual([
+      'i',
+      'love',
+      'tacos',
+    ]);
   });
 });
